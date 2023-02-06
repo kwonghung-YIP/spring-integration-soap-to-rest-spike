b9a7c62cecff0cd7fad613c5f9721a2d02992297
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2023-02-06 11:40:52 +0000

    add currency name call

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ watch -n 1 curl -u hung:pass1234 -v -s http://localhost:8080/api/echo\?ping\=hun
 curl -u hung:pass1234 -v -s http://localhost:8080/api/upper\?value\=hung
 ```
 
+```bash
+curl -u hung:pass1234 -v -s http://localhost:8080/api/currencyname\?code\=HKD
+```
--- modules/rest-to-soap/src/main/java/poc/restToSoap/web/SimpleRestController.java
+++ modules/rest-to-soap/src/main/java/poc/restToSoap/web/SimpleRestController.java
@@ -1,21 +1,14 @@
 package poc.restToSoap.web;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.annotation.Gateway;
-import org.springframework.integration.annotation.MessagingGateway;
-import org.springframework.integration.dsl.MessageChannels;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import poc.restToSoap.integration.IntegrationFlowConfig.GatewayAsFunction;
+import poc.restToSoap.svc.DelegatedService;
 
 @Slf4j
 @RestController
@@ -24,6 +17,9 @@ public class SimpleRestController {
 
     @Autowired
     private DelegatedService service;
+    
+    @Autowired
+    private GatewayAsFunction gateway;
 
     @GetMapping("/echo")
     public String echo(@RequestParam String ping) {
@@ -35,35 +31,13 @@ public class SimpleRestController {
     @GetMapping("/upper")
     public String toUppercase(@RequestParam String value) {
         log.info("incoming request {}",value);
-        return service.toUppercase(value);
+        return gateway.toUppercase(value);
     }
 
-    @Component
-    static public class DelegatedService {
-
-        final private MessageChannel msgChannel;
-        final private FuncAsGateway gateway;
-
-        public DelegatedService(@Qualifier("myChannel") MessageChannel msgChannel,FuncAsGateway gateway) {
-            this.msgChannel = msgChannel;
-            this.gateway = gateway;
-        }
-
-        public void apply(String payload) {
-            Message msg = MessageBuilder.withPayload(payload).build();
-            msgChannel.send(msg);
-        }
-
-        public String toUppercase(String payload) {
-            return this.gateway.toUppercase(payload);
-        }
-
+    @GetMapping("/currencyname")
+    public String getCurrencyName(@RequestParam(name="code") String currencyCode) {
+        log.info("incoming request {}",currencyCode);
+        return gateway.getCurrencyName(currencyCode);
     }
 
-    @MessagingGateway(name="myGateway")
-    static public interface FuncAsGateway {
-
-        @Gateway(requestChannel = "gatewayRequest", replyTimeout = 2000, replyChannel = "gatewayReply")
-        public String toUppercase(@Payload String payload);
-    }
 }
